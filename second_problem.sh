afa6a60f18de41296466e051110340568979abb3
sphoujdar <sm.phoujdar@gmail.com>
2020-11-09 17:26:25 +0530

    Add : Multiple Flips and Score

--- second_problem.sh
+++ second_problem.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -x
 
-((flip=RANDOM%2))
+read -p "Enter number of times you want to flip the coin : " flip_number
 
-if [ $flip -eq 0 ] ; then
-	echo Heads
-else
-	echo Tails
-fi
+while [ $flip_number -ne 0 ] ; do
+	((flip=RANDOM%2))
+	if [ $flip -eq 0 ] ; then
+		((heads_count+=1))
+	else
+		((tails_count+=1))
+	fi
+	((flip_number-=1))
+done
 
+echo Heads Score : $heads_count --- Tails Score : $tails_count
