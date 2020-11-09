25817c19be8c5818bd7b89af092ba8fa84db6022
sphoujdar <sm.phoujdar@gmail.com>
2020-11-09 17:41:17 +0530

    Add : Win Logic

--- second_problem.sh
+++ second_problem.sh
@@ -1,15 +1,22 @@
 #!/bin/bash -x
 
-read -p "Enter number of times you want to flip the coin : " flip_number
-
-while [ $flip_number -ne 0 ] ; do
+heads_count=0
+tails_count=0
+while [ $heads_count -ne 21 -a $tails_count -ne 21 ] ; do
 	((flip=RANDOM%2))
 	if [ $flip -eq 0 ] ; then
 		((heads_count+=1))
 	else
 		((tails_count+=1))
 	fi
-	((flip_number-=1))
 done
+
+if [ $heads_count -eq 21 ] ; then
+	echo Heads WON!
+	echo Head Won by $(($heads_count-$tails_count))
+elif [ $tails_count -eq 21 ] ; then
+	echo Tails WON!
+        echo Tails Won by $(($tails_count-$heads_count))
+fi
 
 echo Heads Score : $heads_count --- Tails Score : $tails_count
